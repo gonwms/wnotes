7b57d9734742422e3c64d7de6a456ffdc8321ab6
gonwms <gon.williams@gmail.com>
2019-01-31 17:32:11 -0300

    aaa

--- js/content.js
+++ js/content.js
@@ -125,7 +125,7 @@ var htmlPluck = (function () {
 	}
 
 	function htmlEntities(str) {
-		return str.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;').replace(/'/g,'&apos;');
+		return str.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;'); //.replace(/'/g,'&apos;')
 		
 	}	
 	
